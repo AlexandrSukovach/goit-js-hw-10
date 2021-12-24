d20952aef66e126b87a1603a6bd8a81e7594d29c
Aleksandr Sukovach <alexsukovach@gmail.com>
2021-12-24 17:24:58 +0200

    task-01

---
+++ src/fetchCountries.js
@@ -0,0 +1,16 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// ============запрос===============
+function fetchCountries(name) {
+   return fetch(`https://restcountries.com/v3.1/name/${name}`)
+      .then(response => {
+         // ========mistake-404==========
+         if (response.ok === false) {
+            Notify.failure('Oops, there is no country with that name');
+         }
+         // console.log(response.json())
+         return response.json();
+      });
+};
+
+export default { fetchCountries };
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
 import './css/styles.css';
+import countryCardTpl from './templates/country-card.hbs';
+import countryCardTp2 from './templates/countr-car-al.hbs';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { debounce } from "lodash";
+import API from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+// =============================
+const refs = {
+   cardContainer: document.querySelector(".country-info"),
+   searchBox: document.querySelector('#search-box'),
+   listCountry: document.querySelector(".country-list"),
+};
+
+// ==свойство=debounce================== 
+refs.searchBox.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(e) {
+   e.preventDefault();
+   let namberInput = refs.searchBox.value.length;
+   let wordInput = refs.searchBox.value;
+   // ==свойство=trim()================== 
+   let searchQuery = wordInput.trim()
+   // console.log('namberInput-', namberInput)
+   // console.log('wordInput.trim-', wordInput.trim())
+   // console.log('searchQuery-', searchQuery)
+   if (namberInput < 2) {
+      Notify.info('Too many matches found. Please enter a more specific name.');
+      API.fetchCountries(searchQuery)
+         .then(rendercardContainer)
+         .catch(error => {
+            console.log(error)
+         });
+   } else {
+      API.fetchCountries(searchQuery)
+         .then(rendercardContainer)
+         .catch(error => {
+            console.log(error)
+         });
+   };
+};
+
+// =====render============
+function rendercardContainer(country) {
+   let resp = country;
+   // console.log(resp.length)
+   if (resp.length === 1) {
+      let markup = countryCardTpl(country);
+      refs.cardContainer.innerHTML = markup;
+      // console.log(markup)
+   } else if (resp.length > 1 && resp.length <= 10) {
+      let markup2 = countryCardTp2(country);
+      refs.cardContainer.innerHTML = markup2;
+      // console.log(markup2)
+   } else if (resp.length === undefined) {
+      let markup2 = countryCardTp2(country);
+      refs.cardContainer.innerHTML = markup2;
+   }
+};
