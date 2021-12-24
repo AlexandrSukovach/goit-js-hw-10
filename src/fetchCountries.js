d3c690f7a1faf62425b9f8a17dba536ec14650ac
Aleksandr Sukovach <alexsukovach@gmail.com>
2021-12-24 21:44:14 +0200

    исправление запросов

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,7 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 // ============запрос===============
 function fetchCountries(name) {
-   return fetch(`https://restcountries.com/v3.1/name/${name}`)
+   return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
       .then(response => {
          // ========mistake-404==========
          if (response.ok === false) {
